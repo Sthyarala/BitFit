416d49b7645adc997dd08d5145c5fa60e6b3cd4b
Sthyarala <sthyarala@gmail.com>
2022-10-11 03:56:43 -0400

    update

---
+++ app/src/main/java/com/example/bitfit/ConfirmDeleteActivity.java
@@ -0,0 +1,6 @@
+package com.example.bitfit;
+
+import android.app.Activity;
+
+public class ConfirmDeleteActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/bitfit/DetailsActivity.java
@@ -0,0 +1,6 @@
+package com.example.bitfit;
+
+import android.app.Activity;
+
+public class DetailsActivity extends Activity {
+}
